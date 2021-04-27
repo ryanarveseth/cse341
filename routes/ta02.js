978fdccb93a65dd5becdf3892dacadb3dda21e19
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2021-04-26 18:59:24 -0600

    TA02

--- routes/ta02.js
+++ routes/ta02.js
@@ -2,14 +2,42 @@
 // Remember, you can make more of these placeholders yourself! 
 const express = require('express');
 const router = express.Router();
+let users = ['Eugene', 'Christine', 'Yu-Chun'];
+let errorMessage = '';
 
 router.get('/',(req, res, next) => {
-    res.render('pages', { 
-        title: 'Team Activity 02', 
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+        contentCSS: true, // For HBS,
+        users: users,
+        errorMessage: errorMessage
     });
+    errorMessage = '';
+});
+
+router.post('/addUser', (req, res, next) => {
+    const {user} = req.body;
+
+    if (!users.includes(user)) {
+        users.push(user);
+    } else {
+        errorMessage = `User '${user}' could not be added because they already exist.`
+    }
+    res.redirect('/ta02/', 302);
+});
+
+router.post('/removeUser', (req, res) => {
+    const lengthBefore = users.length;
+    users = users.filter(user => user !== req.body.user);
+    const lengthAfter = users.length;
+
+    if (lengthBefore === lengthAfter) {
+        errorMessage = `User '${req.body.user}' could not be found`;
+    }
+
+    res.redirect('/ta02/', 302);
 });
 
 module.exports = router;
