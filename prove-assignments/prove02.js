092bbd7a2a2226d9c39b6ae73ee58107a8e42282
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2021-04-29 23:14:12 -0600

    Prove02

--- prove-assignments/prove02.js
+++ prove-assignments/prove02.js
@@ -1,18 +1,14 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
-const users = [];
-
-const routes = require('./routes/prove02');
-
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(routes);
-
-app.use((req, res) => {
-  res.status(404)
-    .send('<h1>404 - Not Found</h1>')
-});
-
-app.use(express.static(path.join(__dirname, 'stylesheets','prove02.css')));
+const prove02Routes = require("./routes/prove02");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 app.listen(3000);
+
+app
+  .use(express.static(__dirname + '/public'))
+  .use(bodyParser({extended: false}))
+  .set('view engine', 'ejs')
+  .set('views', path.join(__dirname, 'views'))
+  .use(prove02Routes);
--- prove-assignments/routes/prove02.js
+++ prove-assignments/routes/prove02.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
 
-router.get('/some-get', (req, res, next) => {
+const books = [];
+let showForm = true;
 
+router.get('/', (req, res) => {
+  showForm = true;
+  res.render('prove02', {
+    books: books,
+    path: '/prove02',
+    title: 'Add a Book',
+    showForm: showForm
+  });
 });
 
-router.post('/some-post', (req, res, next) => {
+router.post('/add-book', (req, res) => {
 
+  const {bookTitle, bookSummary} = req.body;
+  console.log('request.body', req.body);
+  books.push({bookTitle: bookTitle, bookSummary: bookSummary});
+  showForm = false;
+
+  res.render('prove02', {
+    books: books,
+    title: `New Book: ${bookTitle}`,
+    path: '/prove02',
+    showForm: showForm
+  });
 });
 
-router.get('/', (req, res, next) => {
-  res.send('<h1>Home page</h1>');
+router.use((req, res) => {
+  res.status(404)
+    .send('<h1>404 - Not Found</h1>')
 });
-
 
 
 
