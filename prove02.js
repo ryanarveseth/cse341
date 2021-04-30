964d4977cb4e3a00d302385f8a3c24a64481ec3d
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2021-04-30 00:08:29 -0600

    fix prove02

--- prove02.js
+++ prove02.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const prove02Routes = require("./routes/prove02");
 const path = require("path");
+const bodyParser = require('body-parser');
 
 app.listen(process.env.PORT || 3000);
 
 app
+  .use(bodyParser({extended: false}))
   .use(express.static(__dirname + '/public'))
   .set('view engine', 'ejs')
   .set('views', path.join(__dirname, 'views'))
