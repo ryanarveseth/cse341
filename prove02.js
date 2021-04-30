8bbc6c01e41e907404e181d776dd7ad282d0da33
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2021-04-30 00:03:35 -0600

    fix procfile

--- prove02.js
+++ prove02.js
@@ -3,7 +3,7 @@ const app = express();
 const prove02Routes = require("./routes/prove02");
 const path = require("path");
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 app
   .use(express.static(__dirname + '/public'))
