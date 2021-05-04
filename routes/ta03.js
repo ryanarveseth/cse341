3cb1b191953a2805b4eb79063c2b7537975f5bc4
Arveseth-Ryan <54163533+ryanarveseth@users.noreply.github.com>
2021-05-03 19:27:35 -0600

    TA03

--- routes/ta03.js
+++ routes/ta03.js
@@ -1,13 +1,35 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const https = require('https');
 
-router.get('/',(req, res, next) => {
-    res.render('pages/ta03', { 
-        title: 'Team Activity 03', 
-        path: '/ta03', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+let items = [];
+let searchValue = '';
+
+router.post('/search', (req, res, next) => {
+    searchValue = req.body.searchValue;
+    res.redirect('/ta03/', 302);
+});
+
+router.get('/', (req, res, next) => {
+    https.get('https://byui-cse.github.io/cse341-course/lesson03/items.json', (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            items = (JSON.parse(data));
+            res.render('pages/ta03', {
+                title: 'Team Activity 03',
+                path: '/ta03', // For pug, EJS
+                activeTA03: true, // For HBS
+                contentCSS: true, // For HBS
+                items: items,
+                searchValue: searchValue
+            });
+        });
     });
 });
 
